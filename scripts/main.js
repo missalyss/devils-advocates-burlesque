0fb6edcf2a3979882e3661234d7f81679c3bca7f
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-03-13 16:23:51 -0700

    zeroing in on making ticket stuff

--- scripts/main.js
+++ scripts/main.js
@@ -7,24 +7,22 @@ $(document).ready(function() {
 
 //UPCOMING SHOWS
 var upcoming = [{
-        img: "http://fillmurray.com/300/300",
-        imgAlt: "this is picture",
-        title: "Den of Indecency",
-        date: "March 3, 11:00pm",
+        img: `<iframe width="560" height="315" src="https://www.youtube.com/embed/vAB2KSiBn-s" frameborder="0" allowfullscreen></iframe>`,
+        imgAlt: "trying to put in a video",
+        title: "Beauty in Bedlam Trailer",
+        subtitle: "Our show last November",
         about: "Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter."
     },
     {
-        img: "http://fillmurray.com/400/300",
-        imgAlt: "this is picture",
-        title: "Flora and Fauna",
-        date: "April 8, 9, 10",
+        img: `<img src="http://fillmurray.com/400/300" alt="this is picture">`,
+        title: "Don't miss our upcoming show: Flora and Fauna!",
+        subtitle: "April 8, 9, 10",
         about: "Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter."
     },
     {
-        img: "http://fillmurray.com/350/350",
-        imgAlt: "this is picture",
-        title: "Guilty Pleasures",
-        date: "May 5 11:00pm",
+        img: `<img src="http://fillmurray.com/350/350" alt="this is picture">`,
+        title: "Devil's Advocates are thinking of starting a Cabaret. Wanna get on board?",
+        subtitle: "Send us an email today!",
         about: "Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter."
     }
 
@@ -40,7 +38,7 @@ var br = `</br>`
 //APPEND OBJECTS INTO MAIN BODY
 function appendArticle() {
     for (var i = 0; i < upcoming.length; i++) {
-        $('.home-main').append(rowOpen + pictureColOpen + `<img src="${upcoming[i].img}" alt="${upcoming[i].imgAlt}">` + divClose + articleColOpen + `<h3>${upcoming[i].title}</h3>` + `<h5>${upcoming[i].date}</h5>` + `<p>${upcoming[i].about}</p>` + divClose + divClose + `<div class="divider"></div>` + br)
+        $('.home-main').append(rowOpen + pictureColOpen + upcoming[i].img + divClose + articleColOpen + `<h4>${upcoming[i].title}</h4>` + `<h5>${upcoming[i].subtitle}</h5>` + `<p>${upcoming[i].about}</p>` + divClose + divClose + `<div class="divider"></div>` + br)
     }
 }
 
---
+++ scripts/shows.js
@@ -0,0 +1,52 @@
+console.log("sanity!!!!!!!!!");
+
+// ACTIVATES DROPDOWN MENU ON HOMEPAGE
+$(document).ready(function() {
+    $(".dropdown-button").dropdown();
+})
+
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('#modal1').modal();
+});
+
+//UPCOMING SHOWS
+var upcoming = [{
+        img: "http://fillmurray.com/300/300",
+        imgAlt: "this is picture",
+        title: "Den of Indecency",
+        date: "March 3, 11:00pm",
+        about: "Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter."
+    },
+    {
+        img: "http://fillmurray.com/400/300",
+        imgAlt: "this is picture",
+        title: "Flora and Fauna",
+        date: "April 8, 9, 10",
+        about: "Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter."
+    },
+    {
+        img: "http://fillmurray.com/350/350",
+        imgAlt: "this is picture",
+        title: "Guilty Pleasures",
+        date: "May 5 11:00pm",
+        about: "Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter."
+    }
+
+]
+
+
+var rowOpen = `<div class="row">`
+var pictureColOpen = `<div class="col l5 s12">`
+var articleColOpen = `<div class="col l7 s12">`
+var divClose = `</div>`
+var br = `</br>`
+
+//APPEND OBJECTS INTO MAIN BODY
+function appendArticle() {
+    for (var i = 0; i < upcoming.length; i++) {
+        $('.shows-main').append(rowOpen + pictureColOpen + `<img src="${upcoming[i].img}" alt="${upcoming[i].imgAlt}">` + divClose + articleColOpen + `<h3>${upcoming[i].title}</h3>` + `<h5>${upcoming[i].date}</h5>` + `<p>${upcoming[i].about}</p>` + `<a href="../html/tickets.html">Buy tickets here!</a>`+divClose + divClose + `<div class="divider"></div>` + br)
+    }
+}
+
+// appendArticle()

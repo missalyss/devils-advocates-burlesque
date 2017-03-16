bacfde97b581c298109d40de46770aee9c379bca
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-03-15 18:30:51 -0700

    social media logos and some other styling

--- scripts/tickets.js
+++ scripts/tickets.js
@@ -1,4 +1,3 @@
-
 //Eventually, this page will become a legitimate checkout when I have a server for this. subResult will get saved in the server and transfer to this page. $('#total').replaceWith(subResult) basically.
 
 //LOL I'M NOT SURE WHAT THIS IS
@@ -11,51 +10,42 @@
 //    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
 //  });
 
+console.log($('#tix-quantity'));
+
+$('#email-setup').on('submit', function(e) {
+    e.preventDefault()
+    var $email = $('#email').val()
+    var $firstName = $('#first_name').val()
+    var $lastName = $('#last_name').val()
 
 
- // $('#email-setup').on('submit', function(e){
- //     e.preventDefault()
- //     var email = $('#email').val()
-
- $.ajax({
-   url: 'http://galvanize-cors-proxy.herokuapp.com/https://api.mailgun.net/v3/sandbox2fe140c527144a9285414c87b24372b2.mailgun.org/messages',
-  //  username: 'api',
-  //  password: 'api:key-eda7e95057a36fdee3ca6f8beb90a734',
-   method: "POST",
-   dataType: 'json',
-   data: {
-     from: 'Mailgun Sandbox <postmaster@sandbox2fe140c527144a9285414c87b24372b2.mailgun.org>',
-     to: 'Alyssa <alyssa.m.evans@gmail.com>',
-     subject: 'cool beans',
-     text: 'Hello things and stuff is rad dope'
-   },
-   beforeSend: function (xhr) {
-     xhr.setRequestHeader('Authorization', 'Basic ' + btoa('api:key-eda7e95057a36fdee3ca6f8beb90a734'))
-   }
- }).then(function (result) {
-   console.log(result);
- }).catch(function(error) {
-   console.log('Errorrrrrrrrrrrrrr:', error); //also can console.error
- })
- // })
-
-
- // $.ajax({
- //     headers: {"Authorization": "Basic api:key-b50a9065a7b9bdf464f3d7a418ca96bb"},
- //
- //     url: "https://api.mailgun.net/v3/sandbox93d8299f673a4c32952b7592956cb3d9.mailgun.org/messages",
- //     type: "POST",
- //     dataType: 'json',
- //     data: {
- //         name: name,
- //         phone: phone,
- //         email: email,
- //         message: message
- //     },
- //     success: function() {
- //         alert('ok');
- //     },
- //     error: function() {
- //         alert('problems');
- //     }
- // }
+    $.ajax({
+        url: 'http://galvanize-cors-proxy.herokuapp.com/https://api.mailgun.net/v3/sandbox2fe140c527144a9285414c87b24372b2.mailgun.org/messages',
+        method: "POST",
+        dataType: 'json',
+        data: {
+            from: 'Mailgun Sandbox <postmaster@sandbox2fe140c527144a9285414c87b24372b2.mailgun.org>',
+            to: $email,
+            subject: 'Tickets for Devil\'s Advocates Burlesque',
+            text: $firstName + ' ' + $lastName + ',\n\
+            \n\
+            Thank you for your recent purchase to Devil\'s Advocates upcoming show, "Flora and Fauna."\n\
+            You have purchased\n\
+            2 tickets for \n\
+            April 7 at 8:00pm\n\
+            Total: $34 \n\
+            Please keep this email, as this is your receipt and ticket into the show. \n\
+            We are excited to see you there! \n\
+            \n\
+            With love,\n\
+            The Devil\'s Advocates'
+        },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Authorization', 'Basic ' + btoa('api:key-eda7e95057a36fdee3ca6f8beb90a734'))
+        }
+    }).then(function(result) {
+        console.log(result);
+    }).catch(function(error) {
+        console.log('Errorrrrrrrrrrrrrr:', error); //also can console.error
+    })
+})

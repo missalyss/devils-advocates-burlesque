ec4d86ebf3cfa1439204c40ea92e329f6751735e
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-03-16 10:10:42 -0700

    Some styling stuff

--- scripts/performers.js
+++ scripts/performers.js
@@ -3,66 +3,88 @@ console.log("sanity cheeeck");
 var cast = [{
         img: "./images/burlesque/WillyNilly2-cropped.jpg",
         imgAlt: "Willy Nilly",
+        facebook: "https://www.facebook.com/willy.nilly.5832",
+        insta: "#",
         name: "Willy Nilly",
         about: "Willy Nilly is a boy next door fantasy you've never had. After running away from his stuffy upper crust life, Willy spends his time dancing in the shadows telling fortunes and judging your taste in shoes."
     },
     {
         img: "./images/burlesque/Hersera2-cropped.jpg",
         imgAlt: "Hersera",
+        facebook: "https://www.facebook.com/profile.php?id=100012557238044",
+        insta: "#",
         name: "Hersera",
         about: "Hersera is a sultry vagabond who's picked up more than a trick or two in her wanderings. Slipping between world's, she's learned anything from the art of belly dance to the perfect cup of tea, and even a little magic. Come see this mysterious, lonely traveler to get a taste of her eclectic charms."
     },
     {
         img: "./images/burlesque/Boozy2-cropped.jpg",
         imgAlt: "Boozy Cheeks",
+        facebook: "https://www.facebook.com/boozy.cheeks",
+        insta: "#",
         name: "Boozy Cheeks",
         about: "Miss Boozy Cheeks, Seattle's 'Tipsy Tease,' was born to cover her curves in glitter and stumble across stages. Boozy loves long walks on the beach, cuddling, and practically anything soaked in liquor. Since graduating from the Cherry Manhattan School of Burlesque, Miss Boozy has worked with Whiplash's Vicious Vixens, Beaucoup Productions, OK FINE Collaborations and is a member of Seattle's newest burlesque troupe, The Devil's Advocates. Catch a glimpse of Boozy in The Devil's Advocates next monthly show, Miss Effie DuBois' Den of Indecency at The Rendezvous Jewel Box Theatre Oct 7!"
     },
     {
         img: "./images/burlesque/Gin-cropped.jpg",
         imgAlt: "Gin Atomic",
+        facebook: "#",
+        insta: "#",
         name: "Gin Atomic",
         about: "Gin Atomic is not just your average white trash, seductive, gaudy women who likes to kick her legs way above her head, no, no, no.  She provides just the right amount of spice, promiscuity and unattractive facial expressions to make any audience member question their idea of sexy. Inspired by women who don't give a fuck, the over use of red lip stick, and feminists everywhere, Gin Atomic is anything but average."
     },
     {
         img: "./images/burlesque/Foxx5-cropped.jpg",
         imgAlt: "StarFoxx LaCloud",
+        facebook: "#",
+        insta: "#",
         name: "StarFoxx LaCloud",
         about: "Foxx LaCloud is goofy AF. A shameless geek, Foxx is always down for a good time, whether it’s a night-in of Star Trek And Chill, or energetically failing at bowling. Her playful nature may make her seem carefree and naïve, but don’t think for a minute that she’s gonna take your shit. A member of Devil’s Advocates Burlesque Troupe, come watch Foxx get silly and sexy this October 7th at the Rendezvous Jewel Box Theatre in “Miss Effie Dubois’ Den of Indecency”!"
     },
     {
         img: "./images/burlesque/Malibu2-cropped.jpg",
         imgAlt: "Malibu Bambi",
+        facebook: "#",
+        insta: "#",
         name: "Malibu Bambi",
         about: "'Come on Barbie lets go party!' Not your average bimbo-Malibu Bambi is the kind of Barbie who will leave you with tingles. This ultra feminine diva kicked Ken to the curb and is on the hunt for her next play thing. Just like there are two sides to every coin, Malibu may be flirting and dancing one second, and kicking ass the next. Don't mess with this princess in pink."
     },
     {
         img: "./images/burlesque/Jax-cropped.jpg",
         imgAlt: "Jax Mourningwood",
+        facebook: "#",
+        insta: "#",
         name: "Jax Mourningwood",
         about: "Jax Mourningwood is a super-flexible Lumbersexual certain to leave you with wood. Somewhere between dungeon kink and showboi glamour, he always aims to please. Don't miss out on this flannel-flocked fella and his friend Miss Effie DuBois' Den of Indecency October 7th!"
     },
     {
         img: "./images/burlesque/Delica-cropped.jpg",
         imgAlt: "Delicatessen",
+        facebook: "#",
+        insta: "#",
         name: "Delicatessen",
         about: "Delicatessen, the dame of deliciousness is a damsel of the Jazz Age. She jumped on a circus train in 1924 and never looked back. Harkening back to a time where taboos were dangerous and the lustre of a traveling circus was the best entertainment around, she will tiptoe to the edge of indecency, dancing right on your edge. Let her delight you with her high-kicks and fancy feet then scintillate your senses with silky swivels. However it's not all rhinestones and feather boas - there's a scrappy circus carny hiding in there, waiting to show you something dark, sensual and forbidden from the Peep Show Tent."
     },
     {
         img: "./images/burlesque/Tout-cropped.jpg",
         imgAlt: "Tout d'Lou",
+        facebook: "#",
+        insta: "#",
         name: "Tout d'Lou",
         about: "Tout d'Lou is a runaway mime child on the loose! At the young age of 13, he ran away from home in order to join the circus to share his talent of getting into trouble with the world. Throughout his travels he ran into the worlds most beautiful creatures, who taught him that body language could carry on a conversation better than words every could. Now with his new found talents, he can trap you in a box you never want to find your way out of!"
     },
     {
         img: "./images/burlesque/Mystic-cropped.jpg",
         imgAlt: "Mystic Deflowered",
+        facebook: "#",
+        insta: "#",
         name: "Mystic Deflowered",
         about: "Mystic Deflowered is a psychedelic daydream--strung out and ready for adventure. Although her background is a full life in dance and theatre, she gains inspiration from creating visual art and attempting to communicate her deepest, strangest visions. She most recently produced Tease the Season: a Sexy Summer Hotliday Extravaganza and Vices: a Burlesque, commonly performs with the troupe Lady's Luck, and is thrilled to have a new family with the Devil's Advocates."
     },
     {
         img: "./images/burlesque/Effie-cropped.jpg",
         imgAlt: "Effie DuBois",
+        facebook: "#",
+        insta: "#",
         name: "Effie DuBois",
         about: "Miss Effie DuBois: A child of the Jazz age, lover of Bohemia, and your resident bad poet. Born out of the under belly of the golden age of America, she's the perfect combination of tragic starlet, debutante and liquor influenced bad decisions. Also the principal dancer for Beacoup's Productions, you can catch her shedding a few tears and a few articles of clothing alongside her glitter family within the Devils Advocates!"
     }
@@ -77,10 +99,12 @@ var articleColOpen = `<div class="col l7 s12">`
 var divClose = `</div>`
 var br = `</br>`
 
+
 //APPEND OBJECTS INTO MAIN BODY
 function appendArticle() {
     for (var i = 0; i < cast.length; i++) {
-        $('.performer-main').append(rowOpen + pictureColOpen + `<img src="${cast[i].img}" alt="${cast[i].imgAlt}">` + divClose + articleColOpen + `<h3>${cast[i].name}</h3>` + `<p>${cast[i].about}</p>` + divClose + divClose + `<div class="divider"></div>` + br)
+        $('.performer-main').append(rowOpen + pictureColOpen + `<img src="${cast[i].img}" alt="${cast[i].imgAlt}">` + divClose + articleColOpen + `<h3>${cast[i].name}</h3>` + `<p>${cast[i].about}</p>` +
+        `<p><a href="${cast[i].facebook}"><img src="./images/facebook-icon.png" alt="${cast[i].name} Facebook"></a><a href="${cast[i].insta}"><img src="./images/instagram-icon.png" alt="${cast[i].name} Instagram"></a></p>` + divClose + divClose + `<div class="divider"></div>` + br)
     }
 }
 
